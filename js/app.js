3c77ebf81fde377e834a5613037cfd0f61597f41
Marco Ancona <marco.ancona@inf.ethz.ch>
2017-07-26 16:40:28 +0200

    Commenting out D3.js code

--- js/app.js
+++ js/app.js
@@ -148,83 +148,83 @@ function render(epochNr) {
 }
 
 
-function plot_stats(data) {
-    //Remove old plots
-    d3.select('#avgplot').selectAll("*").remove();
-    d3.select('#varplot').selectAll("*").remove();
-
-    //Grab svg elements
-    var avgsvg = d3.select('#avgplot');
-    var varsvg = d3.select('#varplot');
-
-    //Calculate stats to display
-    var statData = calc_stats(data);
-
-    //Console dump for debugging purposes
-    /*console.log("StatData:\n" +
-        "Index; Average; Variance\n" +
-        statData[0][0] + "; " + statData[0][1] + "; " + statData[0][2] + "\n" +
-        statData[1][0] + "; " + statData[1][1] + "; " + statData[1][2] + "\n" +
-        statData[2][0] + "; " + statData[2][1] + "; " + statData[2][2] + "\n" +
-        "...");
-    */
-
-    //Draw the graphs
-    drawHorizontalGraph(avgsvg, statData, "AVG", 1, "steelblue");
-    drawHorizontalGraph(varsvg, statData, "VAR", 2, "coral");
-}
-
-function drawHorizontalGraph (svg, data, name, colIndex, color) {
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    //console.log("Graph size: " + width + "x" + height);
-
-    var x = d3.scaleLinear()
-        .domain([0, data.length])
-        .range([0, width]);
-
-    var y = d3.scaleLinear()
-        .domain([-0.03, 0.03])
-        .range([height, 0]);
-
-    var line = d3.line()
-        .x(function(d) { return x(d[0]); })
-        .y(function(d) { return y(d[colIndex]); });
-
-    //X Axis
-    g.append("g")
-        .attr('class', 'x axis')
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).ticks(4))
-        .select(".domain")
-        .remove();
-
-    //Y Axis
-    g.append("g")
-        .call(d3.axisLeft(y).ticks(3))
-        .append("text")
-        .attr("fill", "#000")
-        //.attr("transform", "rotate(-90)")
-        .attr("y", -15)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text(name);
-
-    //Graph
-    g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", color)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
-
-}
+// function plot_stats(data) {
+//     //Remove old plots
+//     d3.select('#avgplot').selectAll("*").remove();
+//     d3.select('#varplot').selectAll("*").remove();
+//
+//     //Grab svg elements
+//     var avgsvg = d3.select('#avgplot');
+//     var varsvg = d3.select('#varplot');
+//
+//     //Calculate stats to display
+//     var statData = calc_stats(data);
+//
+//     //Console dump for debugging purposes
+//     /*console.log("StatData:\n" +
+//         "Index; Average; Variance\n" +
+//         statData[0][0] + "; " + statData[0][1] + "; " + statData[0][2] + "\n" +
+//         statData[1][0] + "; " + statData[1][1] + "; " + statData[1][2] + "\n" +
+//         statData[2][0] + "; " + statData[2][1] + "; " + statData[2][2] + "\n" +
+//         "...");
+//     */
+//
+//     //Draw the graphs
+//     drawHorizontalGraph(avgsvg, statData, "AVG", 1, "steelblue");
+//     drawHorizontalGraph(varsvg, statData, "VAR", 2, "coral");
+// }
+//
+// function drawHorizontalGraph (svg, data, name, colIndex, color) {
+//
+//     var margin = {top: 20, right: 20, bottom: 30, left: 50},
+//         width = +svg.attr("width") - margin.left - margin.right,
+//         height = +svg.attr("height") - margin.top - margin.bottom,
+//         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//     //console.log("Graph size: " + width + "x" + height);
+//
+//     var x = d3.scaleLinear()
+//         .domain([0, data.length])
+//         .range([0, width]);
+//
+//     var y = d3.scaleLinear()
+//         .domain([-0.03, 0.03])
+//         .range([height, 0]);
+//
+//     var line = d3.line()
+//         .x(function(d) { return x(d[0]); })
+//         .y(function(d) { return y(d[colIndex]); });
+//
+//     //X Axis
+//     g.append("g")
+//         .attr('class', 'x axis')
+//         .attr("transform", "translate(0," + height + ")")
+//         .call(d3.axisBottom(x).ticks(4))
+//         .select(".domain")
+//         .remove();
+//
+//     //Y Axis
+//     g.append("g")
+//         .call(d3.axisLeft(y).ticks(3))
+//         .append("text")
+//         .attr("fill", "#000")
+//         //.attr("transform", "rotate(-90)")
+//         .attr("y", -15)
+//         .attr("dy", "0.71em")
+//         .attr("text-anchor", "end")
+//         .text(name);
+//
+//     //Graph
+//     g.append("path")
+//         .datum(data)
+//         .attr("fill", "none")
+//         .attr("stroke", color)
+//         .attr("stroke-linejoin", "round")
+//         .attr("stroke-linecap", "round")
+//         .attr("stroke-width", 1.5)
+//         .attr("d", line);
+//
+// }
 
 function calc_stats(data) {
     var dataAmount = data[0].length;
